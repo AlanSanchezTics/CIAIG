74a7986a3d222039a1cf856ed3deb02aeee65f3f
AlanSanchezTics <alan.dsrn@gmail.com>
2018-07-12 00:01:23 -0500

    Act 12:01 12/07/2018

--- cPanel/AvisosPanel/AvisosPersonales.php
+++ cPanel/AvisosPanel/AvisosPersonales.php
@@ -4,7 +4,7 @@
 	$idAlumno = $obj["idAlumno"];
 
 	 include "../../conexion.php";
-	$sql = "SELECT ID_AVISO, TITULO_AVISO, DESCRIPCION_AVISO, FECHA_INICIAL, FECHA_FINAL FROM tbl_avisos_alumno WHERE ID_ALUMNO='{$idAlumno}' AND EXISTE = 1 ORDER BY ID_AVISO DESC";
+	$sql = "SELECT ID_AVISO, TITULO_AVISO, DESCRIPCION_AVISO, FECHA_INICIAL, FECHA_FINAL FROM tbl_avisos_alumno WHERE ID_ALUMNO='{$idAlumno}' AND EXISTE = 1 AND FECHA_FINAL > NOW() ORDER BY ID_AVISO DESC";
 	$result = mysqli_query($conexion,$sql);
 	
 	if($sql){
