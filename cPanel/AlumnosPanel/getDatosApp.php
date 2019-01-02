9b0cd1c6f42df3c48524fcf30668f76045cd15d0
AlanSanchezTics <33034222+AlanSanchezTics@users.noreply.github.com>
2019-01-01 22:02:19 -0600

    01/01/2019

--- cPanel/AlumnosPanel/getDatosApp.php
+++ cPanel/AlumnosPanel/getDatosApp.php
@@ -1,9 +1,12 @@
 <?php
-$json =file_get_contents("php://input");
-$obj = json_decode($json,true);
-$arreglo = array();
-$alumno = $obj['idAlumno'];
-
+if(isset($_POST["idAlumno"])){
+	$alumno = $_POST["idAlumno"];
+}else{
+	$json =file_get_contents("php://input");
+	$obj = json_decode($json,true);
+	$arreglo = array();
+	$alumno = $obj['idAlumno'];
+}
 	include "../../conexion.php";
 	
 	$sql = "SELECT NOMBRE, A_PATERNO, A_MATERNO, GRADO, TEL, EMAIL, NIVEL, FECHA_INGRESO, FECHA_EGRESO, IMAGEN FROM tbl_alumnos WHERE ID_ALUMNO='{$alumno}' AND tbl_alumnos.EXISTE= 1";
--- dPanel/Tareas/GetTareas.php
+++ dPanel/Tareas/GetTareas.php
@@ -1,8 +1,13 @@
 <?php
-    $json =file_get_contents("php://input");
-	$obj = json_decode($json,true);
-    $grupo = $obj["grupo"];
-    $tipo = $obj["tipo"];
+    if(isset($_POST["grupo"]) && isset($_POST["tipo"])){
+		$grupo = $_POST["grupo"];
+		$tipo = $_POST["tipo"];
+	}else{
+		$json =file_get_contents("php://input");
+		$obj = json_decode($json,true);
+    	$grupo = $obj["grupo"];
+    	$tipo = $obj["tipo"];
+	}
 	 include "../../conexion.php";
     $sql = "SELECT ID_TAREA, TITULO_TAREA, DESCRIPCION_TAREA, FECHA_CREACION, FECHA_ENTREGA, tbl_docentes.NOMBRE, tbl_docentes.A_PATERNO FROM tbl_tareas, tbl_docentes WHERE ID_GRUPO = {$grupo} AND TIPO_TAREA = '{$tipo}' AND tbl_tareas.ID_DOCENTE = tbl_docentes.ID_DOCENTE AND tbl_tareas.FECHA_ENTREGA >= CURRENT_TIMESTAMP AND tbl_tareas.EXISTE = 1 ORDER BY ID_TAREA DESC";
     $result = mysqli_query($conexion,$sql);
